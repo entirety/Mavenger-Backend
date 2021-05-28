93cf022bf2c21e26d2f7be6a7baa9ca659dbcb08
Lebowski <lebowskigh@gmail.com>
2021-05-28 05:24:04 -0400

    Hashing, fixed password exposure, error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 import { UserGroups } from './user-groups.enum';
@@ -11,11 +12,26 @@ export class AuthService {
 
   async createUser(createUserDto: CreateUserDto): Promise<void> {
     const createdUser = new this.UserModel(createUserDto);
+
+    const { password } = createUserDto;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     createdUser.role = UserGroups.UNVERIFIED;
-    await createdUser.save();
+    createdUser.password = hashedPassword;
+    try {
+      await createdUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException();
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   async getUserById(id: string): Promise<User> {
-    return this.UserModel.findById(id);
+    return this.UserModel.findById(id).select('-password');
   }
 }
