964d5ecae938213a73330c1e442ea8fe4133f741
Lebowski <lebowskigh@gmail.com>
2021-05-28 05:26:24 -0400

    More error handling and validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,15 +1,23 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
   readonly name: string;
 
   @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
 
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   readonly username: string;
 
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak', // Handle this better later (more details)
+  })
   readonly password: string;
 }
