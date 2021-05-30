d2e2f81b26fd85e2114ae21469040ce6699a135d
Lebowski <lebowskigh@gmail.com>
2021-05-30 02:22:47 -0400

    Repository design pattern & started refresh tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,40 +1,28 @@
-import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
-import { UserGroups } from './user-groups.enum';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { JwtPayload } from './jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
+import { RefreshToken, RefreshTokenDocumnet } from './schemas/refresh-token.schema';
+import { UsersRepository } from './users.repository';
+import { RefreshTokensRepository } from './refresh-tokens.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name) private readonly UserModel: Model<UserDocument>,
+    @InjectModel(RefreshToken.name) private readonly RefreshModel: Model<RefreshTokenDocumnet>,
+    private readonly usersRepository: UsersRepository,
+    private readonly refreshTokensRepository: RefreshTokensRepository,
     private jwtService: JwtService
   ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<void> {
-    const createdUser = new this.UserModel(createUserDto);
-
-    const { password } = createUserDto;
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    createdUser.role = UserGroups.UNVERIFIED;
-    createdUser.password = hashedPassword;
-    try {
-      await createdUser.save();
-    } catch (error) {
-      if (error.code === 11000) {
-        throw new ConflictException();
-      } else {
-        throw new InternalServerErrorException();
-      }
-    }
+  async signUp(createUserDto: CreateUserDto): Promise<void> {
+    return this.usersRepository.createUser(createUserDto);
   }
 
   async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ token: string }> {
@@ -42,6 +30,12 @@ export class AuthService {
     const user = await this.UserModel.findOne({ username: { $regex: new RegExp(`^${username.toLowerCase()}`, 'i') } });
 
     if (user && (await bcrypt.compare(password, user.password))) {
+      // Test Code
+      const rToken = await this.refreshTokensRepository.createRefreshToken(user.id);
+
+      console.log(rToken);
+      // End Test Code
+
       const payload: JwtPayload = { id: user._id, username };
       const token: string = await this.jwtService.sign(payload);
 
